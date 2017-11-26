a38eb47b69bd993ae8cf6635a3ad1b9689cb257e
whpat126 <124653847@qq.com>
2017-11-26 10:59:09 +0800

    Initial commit

---
+++ src/main/java/tonps/masterspringmvc/MasterspringmvcApplication.java
@@ -0,0 +1,12 @@
+package tonps.masterspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterspringmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterspringmvcApplication.class, args);
+	}
+}
---
+++ src/test/java/tonps/masterspringmvc/MasterspringmvcApplicationTests.java
@@ -0,0 +1,16 @@
+package tonps.masterspringmvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MasterspringmvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
