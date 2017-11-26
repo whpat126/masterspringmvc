4879cfebcf49ef876e96e2451bd0210bf0d97bd6
whpat126 <124653847@qq.com>
2017-11-26 11:04:08 +0800

    增加注释

--- src/main/java/tonps/masterspringmvc/MasterspringmvcApplication.java
+++ src/main/java/tonps/masterspringmvc/MasterspringmvcApplication.java
@@ -3,9 +3,16 @@ package tonps.masterspringmvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * spring boot 启动类
+ */
 @SpringBootApplication
 public class MasterspringmvcApplication {
 
+	/**
+	 * 默认Spring配置
+	 * @param args 默认参数
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(MasterspringmvcApplication.class, args);
 	}
